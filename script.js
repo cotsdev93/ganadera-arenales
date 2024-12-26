9c120543451d2d7aba7babca6df1ff34fbf174fa
cotsdev93 <cotsdev93@gmail.com>
2024-12-26 15:37:16 -0300

    basta por favor

--- script.js
+++ script.js
@@ -396,11 +396,11 @@ class BaseDeDatosProductos {
 
   async cargarRegistros() {
     const resultado = await fetch(
-      "productos.json"
+      "productos.JSON"
     );
 
     this.productos = await resultado.json();
-    // this.productos.sort((a, b) => a.nombre.localeCompare(b.nombre));
+    this.productos.sort((a, b) => a.nombre.localeCompare(b.nombre));
     cargarProductos(this.productos);
     console.log("funca")
   }
